c832f6960a4614c7ba3f7767bfe5eb5b5c6c95e1
Yaroslav <slawik218@gmail.com>
2021-06-20 18:57:59 +0300

    simple login form

---
+++ src/main/java/com/pavliuk/spring/web/MainController.java
@@ -0,0 +1,12 @@
+package com.pavliuk.spring.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/login")
+    public String getLoginPage() {
+        return "login";
+    }
+}
